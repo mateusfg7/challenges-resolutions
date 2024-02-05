057cb3f7172ce573b48de085dc25189be0b7ce4b
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-02-05 13:22:42 -0300

    Add `2024-02-05_who-likes-it` test

---
+++ codewars/2024-02-05_who-likes-it/test.rs
@@ -0,0 +1,23 @@
+#[cfg(test)]
+mod tests {
+    use super::likes;
+
+    #[test]
+    fn example_tests() {
+        assert_eq!(likes(&[]), "no one likes this");
+        assert_eq!(likes(&["Peter"]), "Peter likes this");
+        assert_eq!(likes(&["Jacob", "Alex"]), "Jacob and Alex like this");
+        assert_eq!(
+            likes(&["Max", "John", "Mark"]),
+            "Max, John and Mark like this"
+        );
+        assert_eq!(
+            likes(&["Alex", "Jacob", "Mark", "Max"]),
+            "Alex, Jacob and 2 others like this"
+        );
+        assert_eq!(
+            likes(&["Alex", "Jacob", "Mark", "Max", "Mateus", "Felipe"]),
+            "Alex, Jacob and 4 others like this"
+        );
+    }
+}

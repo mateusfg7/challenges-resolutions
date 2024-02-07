b82540eb717b4d383cacb7c110c027e23cd18441
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-02-07 10:28:41 -0300

    Add `2024-02-07_categorize-new-member` test

---
+++ codewars/2024-02-07_categorize-new-member/test.rs
@@ -0,0 +1,11 @@
+// Add your tests here.
+// See https://doc.rust-lang.org/stable/rust-by-example/testing/unit_testing.html
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn returns_expected() {
+        assert_eq!(open_or_senior(vec![(45, 12), (55,21), (19, -2), (104, 20)]), vec!["Open", "Senior", "Open", "Senior"]);
+        assert_eq!(open_or_senior(vec![(3, 12), (55,1), (91, -2), (54, 23)]), vec!["Open", "Open", "Open", "Open"]);
+    }
+}
